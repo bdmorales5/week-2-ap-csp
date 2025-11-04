65a17b97f40fff01c457e8b150772f504108ca9d
bdmorales5 <bdmorales5@cps.edu>
2025-11-04 18:50:06 +0000

    abstraction

---
+++ abstraction.py
@@ -0,0 +1,23 @@
+# abstraction is when i hide
+# the coomplex details for something
+# alot more simple
+
+
+# personal information
+#a fucntioin allows us to wrap data or
+# information into a special box or
+# enclosure for reuse
+# define a function
+# def personInformation():
+#     question1 = input("How old are you ")
+#     question2 = input("Where do you live? ")
+#     question3 = input("What school do you go to? ")
+    
+#     print(question1 +  + question2 +  + question3)
+
+
+
+q1 = int(input("whay year is it now"))
+q2 = int(input("what year were you born"))
+answer = q1 - q2
+result = print(f'you are {answer} years old')
